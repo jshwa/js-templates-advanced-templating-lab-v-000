a3199ac6e38003a2c267bc6856dc99a3db3751a7
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 01:54:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function init() {
   document.querySelector('main').innerHTML += document.getElementById('recipe-form-template').innerHTML;
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
   Handlebars.registerHelper('displayIngredient', function () {
-    this.value
+    this.value;
   })
 }
 document.addEventListener("DOMContentLoaded", function(event) {
