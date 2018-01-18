425f7cf497f940ccceff72368259331930e29e26
Joshua Palacios <palacios.joshua@gmail.com>
2018-01-18 04:58:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function createRecipe() {
 function displayEditForm() {
   recipe = getRecipe();
   var newForm = Handlebars.compile(document.getElementById('recipe-form-template').innerHTML);
-  document.querySelector('main').innerHTML += newForm({createOrUpdate: "updateRecipe()", name: recipe.name, description: recipe.description, ingredients: recipe.ingredients});
+  document.querySelector('main').innerHTML = newForm({createOrUpdate: "updateRecipe()", name: recipe.name, description: recipe.description, ingredients: recipe.ingredients});
 }
 
 function updateRecipe() {
